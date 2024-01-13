b890fcd3e000411faba31b8c9b39a2d212bc46e4
CoderGabriel <gabrielsanferdev@gmail.com>
2024-01-12 22:06:01 -0300

    terminei o projeto

--- projeto-2/index.js
+++ projeto-2/index.js
@@ -1,6 +1,5 @@
-let resultado = subt(20, 20)
-
-console.log ("o resultado é " + resultado)
+//calculadora
+let saldoWin = subt(1000, 10)
 
 function subt(win, lose){
 
@@ -8,11 +7,27 @@ function subt(win, lose){
     return result;
 }
 
+//patentes
+let nivelHeroi;
 
 
+if(saldoWin < 10){
+    nivelHeroi= "Ferro";
+}else if (saldoWin >= 11 && saldoWin < 21){
+    nivelHeroi = "Bronze";
+}else if (saldoWin >= 21 && saldoWin < 51){
+    nivelHeroi = "Prata";
+}else if (saldoWin >= 51 && saldoWin < 81){
+    nivelHeroi = "Ouro";
+}else if (saldoWin >= 81 && saldoWin < 91){
+    nivelHeroi = "Diamante";
+}else if (saldoWin >= 91 && saldoWin < 101){
+    nivelHeroi = "Lendário";
+}else if (saldoWin >= 101){
+    nivelHeroi = "Imortal";
+}
 
 
-
-
-console.log ("O herói tem saldo de vitórias de " +)
-console.log ( "está no nivel de " )
+//fim do codigo
+console.log ("O herói tem saldo de vitórias de " + saldoWin)
+console.log ( "está no nivel de " + nivelHeroi )
